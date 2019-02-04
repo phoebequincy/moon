644c60644b84165f1e97adf08854b381efbefb6a
Sara Keyser <yogasara@icloud.com>
2019-02-04 15:37:42 -0700

    react materialize set-up and modals craeted from database, need to rough draft

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,37 @@
 import React from 'react'
+import {Button, Modal, Collapsible, CollapsibleItem} from 'react-materialize'
+
 
 export default class Card extends React.Component {
 
   render(){
     return (
 
-      <div className="row">
-         <div className="col s12 m6">
-           <div className="card blue-grey darken-1">
-             <div className="card-content white-text">
-                 <span className="card-title">
-                  {this.props.phase.title}
-                 </span>
-                 <span className="card-title">
-                  {this.props.phase.phase}
-                 </span>
-                 <p>
-                  {this.props.phase.phase}
-                 </p>
-                 <p>
-                  {this.props.phase.description}
-                 </p>
-                 <p>
-                  {this.props.phase.symptomatic_experince}
-                 </p>
-                 <p>
-                  {this.props.phase.mindful_ways}
-                 </p>
-               </div>
-             <div className="card-action">
-               <a href="#">This is a link</a>
-               <a href="#">This is a link</a>
-             </div>
-           </div>
-         </div>
-       </div>
+      <Modal
+        header='Modal Header'
+        trigger={<Button>{this.props.phase.title}</Button>}>
+
+        <Collapsible>
+
+          <CollapsibleItem header={this.props.phase.title} icon='filter_drama'>
+            {this.props.phase.title}
+          </CollapsibleItem>
+
+          <CollapsibleItem header={this.props.phase.phase} icon='place'>
+              {this.props.phase.description}
+          </CollapsibleItem>
+
+          <CollapsibleItem header='Description' icon='whatshot'>
+            {this.props.phase.description}
+          </CollapsibleItem>
+
+          <CollapsibleItem header='Symptomatic Experince' icon='whatshot'>
+            {this.props.phase.symptomatic_experience}
+          </CollapsibleItem>
+
+        </Collapsible>
+
+      </Modal>
      )
    }
 }
