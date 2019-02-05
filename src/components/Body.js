229d2e6ba633a9f54dce9471733833d589136372
SARA MORELL <saramorell@gmail.com>
2019-02-05 01:38:43 -0700

    multi-color hover overlays on moon links

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,15 +11,26 @@ class Body extends Component {
       <div className="bodyDiv">
         <div className="container">
         <div className='circle-container'>
-          <a href='#' className='deg0'><img src='images/waxing-quarter.svg' /><span className="overlay"></span></a>
-          <a href='#' className='deg45'><img src='images/waxing-crescent.svg' /><span className="overlay"></span></a>
-          <a href='#' className='deg90'><img src='images/new.svg' /><span className="overlay"></span></a>
-          <a href='#' class='deg135'><img src='images/waning-crescent.svg' /><span className="overlay"></span></a>
-          <a href='#' class='deg180'><img src='images/waning-quarter.svg' /><span className="overlay"></span></a>
-          <a href='#' class='deg225'><img src='images/waning-gibbous.svg' /><span className="overlay"></span></a>
-          <a href='#' class='deg270'><img src='images/full.svg' /><span className="overlay"></span></a>
-          <a href='#' class='deg315'><img src='images/waxing-gibbous.svg' /><span className="overlay"></span></a>
+          <a href='#' className='deg0'><img src='images/waxing-quarter.svg' /><span className="overlay-pink"></span></a>
+          <a href='#' className='deg45'><img src='images/waxing-crescent.svg' /><span className="overlay-blue"></span></a>
+          <a href='#' className='deg90'><img src='images/new.svg' /><span className="overlay-green"></span></a>
+          <a href='#' class='deg135'><img src='images/waning-crescent.svg' /><span className="overlay-seafoam"></span></a>
+          <a href='#' class='deg180'><img src='images/waning-quarter.svg' /><span className="overlay-coral"></span></a>
+          <a href='#' class='deg225'><img src='images/waning-gibbous.svg' /><span className="overlay-red"></span></a>
+          <a href='#' class='deg270'><img src='images/full.svg' /><span className="overlay-berry"></span></a>
+          <a href='#' class='deg315'><img src='images/waxing-gibbous.svg' /><span className="overlay-lavendar"></span></a>
         </div>
+
+        {/*<div className='circle-container'>
+          <a href='#' className='deg0'><img src='images/waxing-quarter-color.svg' /><span className="overlay"></span></a>
+          <a href='#' className='deg45'><img src='images/waxing-crescent-color.svg' /><span className="overlay"></span></a>
+          <a href='#' className='deg90'><img src='images/new-color.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg135'><img src='images/waning-crescent-color.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg180'><img src='images/waning-quarter-color.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg225'><img src='images/waning-gibbous-color.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg270'><img src='images/full-color.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg315'><img src='images/waxing-gibbous-color.svg' /><span className="overlay"></span></a>
+        </div>*/}
         </div>
         </div>
 
