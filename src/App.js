cd37ccb3d055f1de078bc2e60ff217a61f21768d
Sara Keyser <yogasara@icloud.com>
2019-02-05 14:56:39 -0700

    uncommented api call

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ class App extends Component {
 
   state = {phases:[]}
 
-  // async componentDidMount() {
-  //     const response = await fetch('http://localhost:3000/phases',{mode: 'cors'})
-  //     const json = await response.json()
-  //     this.setState({ phases:json })
-  // }
+  async componentDidMount() {
+      const response = await fetch('http://localhost:3000/phases',{mode: 'cors'})
+      const json = await response.json()
+      this.setState({ phases:json })
+  }
 
   render() {
     return (
       <div className="App">
             <Body />
-            
             <CardContent
             phases ={this.state.phases}
 
