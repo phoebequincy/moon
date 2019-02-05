4855dcdb8d097ba85c9bbebc6b048662f2d7aac9
SARA MORELL <saramorell@gmail.com>
2019-02-05 00:33:24 -0700

    circular moon styling and color overlay on hover

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,56 +10,21 @@ class Body extends Component {
       <div>
       <div className="bodyDiv">
         <div className="container">
-          <div id="row1" className="row valign-wrapper">
-            <div id="block1" className="col l2 offset-l5 center-align ">
-              <img className="moonImage" src="images/full.png" />
-            </div>
-          </div>
-          <div id="row2" className="row">
-            <div className="col l3"></div>
-            <div id="block2" className="col l2 center-align">
-              <img className="moonImage" src="images/waxing-gibbous.png" />
-            </div>
-            <div className="col l2"></div>
-            <div id="block3" className="col l2 center-align">
-              <img className="moonImage" src="images/waning-gibbous.png" />
-            </div>
-            <div className="col l3"></div>
-          </div>
-
-          <div id="row3" className="row">
-          <div className="col l1"></div>
-            <div id="block4" className="col l2 teal center-align">
-              <p>block 4</p>
-            </div>
-            <div className="col l6"></div>
-            <div id="block5" className="col l2 teal center-align">
-              <p>block 5</p>
-            </div>
-            <div className="col l1"></div>
-          </div>
-
-          <div id="row4" className="row">
-            <div className="col l3"></div>
-            <div id="block6" className="col l2 teal center-align">
-              <p>block 6</p>
-            </div>
-            <div className="col l2"></div>
-            <div id="block7" className="col l2 teal center-align">
-              <p>block 7</p>
-            </div>
-            <div className="col l3"></div>
-          </div>
-
-          <div id="row5" className="row">
-            <div id="block8" className="col l2 offset-l5 teal center-align">
-              <p>block 8</p>
-            </div>
-          </div>
+        <div className='circle-container'>
+          <a href='#' className='deg0'><img src='images/waxing-quarter.svg' /><span className="overlay"></span></a>
+          <a href='#' className='deg45'><img src='images/waxing-crescent.svg' /><span className="overlay"></span></a>
+          <a href='#' className='deg90'><img src='images/new.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg135'><img src='images/waning-crescent.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg180'><img src='images/waning-quarter.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg225'><img src='images/waning-gibbous.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg270'><img src='images/full.svg' /><span className="overlay"></span></a>
+          <a href='#' class='deg315'><img src='images/waxing-gibbous.svg' /><span className="overlay"></span></a>
         </div>
         </div>
-        <div className="stars"></div>
-        <div className="twinkling"></div>
+        </div>
+
+          <div className="stars"></div>
+          <div className="twinkling"></div>
       </div>
     )
   }
