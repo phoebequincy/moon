d9f6d96f7f13fdfbe87468966e702bd20f55a741
Phoebe Quincy <phoebequincy@gmail.com>
2019-02-05 09:33:26 -0700

    Minor changes to styling.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
                 <div className="Site-content">
                   <div className="App-header">
                     <Header
-                    
+
                     />
                   </div>
 
