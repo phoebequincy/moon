ad0dff34cf536542c08870b42d97e6d90e710394
Sara Keyser <yogasara@icloud.com>
2019-02-06 13:22:22 -0700

    uninstalled babel and styled components, using react styling with in component css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {Button, Modal, Collapsible, CollapsibleItem} from 'react-materialize'
+import styled from 'styled-components'
 
+const modalStyling = {
+  fontSize: '15px',
+  textAlign: 'left',
+  // background: 'pink'
+}
 
 export default class Card extends React.Component {
 
@@ -8,36 +14,22 @@ export default class Card extends React.Component {
     this.props.phase.id.toString()
   }
 
-
-
   render(){
     return (
-
       <Modal
+      style = {modalStyling}
         header={this.props.phase.title}
         trigger={<a href='#' className={this.props.phase.class}><img src={this.props.phase.img} /><span className={this.props.phase.overlay}></span></a>}
-
         id = {this.idString()}>
-
         <Collapsible>
-
-          <CollapsibleItem className='active' header={this.props.phase.phase} icon='brightness_1'>
-              {this.props.phase.description}
+          <CollapsibleItem className='active' header={this.props.phase.phase}   icon='brightness_1'>{this.props.phase.description}
           </CollapsibleItem>
-
-          <CollapsibleItem header='Symptomatic Experience' icon='brightness_2'>
-            {this.props.phase.symptomatic_experience}
+          <CollapsibleItem header='Symptomatic Experience' icon='brightness_2'>{this.props.phase.symptomatic_experience}
           </CollapsibleItem>
-
-          <CollapsibleItem header='Mindful Ways' icon='brightness_3'>
-
-            {this.props.phase.mindful_ways}
+          <CollapsibleItem header='Mindful Ways' icon='brightness_3'>{this.props.phase.mindful_ways}
           </CollapsibleItem>
-
         </Collapsible>
-
       </Modal>
-
      )
    }
 }
