7835ec1fafa0f1373d732fc8b8877da164f2a23e
Sara Keyser <yogasara@icloud.com>
2019-02-07 13:47:06 -0700

    no changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import {Button, Modal, Collapsible, CollapsibleItem} from 'react-materialize'
 
+const modalStyling = {
+  fontFamily: 'PT Sans',
+  fontSize: '15px',
+  textAlign: 'left',
+}
 
 export default class Card extends React.Component {
 
@@ -8,34 +13,21 @@ export default class Card extends React.Component {
     this.props.phase.id.toString()
   }
 
-
-
   render(){
     return (
-
       <Modal
+        style = {modalStyling}
         header={this.props.phase.title}
-        trigger={<a href='#' className={this.props.phase.class}><img src={this.props.phase.img} /><span className={this.props.phase.overlay}></span></a>}
-
-        id = {this.idString()}>
-
+        trigger={<a href='#' className={this.props.phase.class}><img src={this.props.phase.img}/><span className={this.props.phase.overlay}>
+        </span></a>}id = {this.idString()}>
         <Collapsible>
-
-          <CollapsibleItem className='active' header={this.props.phase.phase} icon='brightness_1'>
-              {this.props.phase.description}
-          </CollapsibleItem>
-
+          <CollapsibleItem className='active' header={this.props.phase.phase} icon='brightness_1'>{this.props.phase.description}</CollapsibleItem>
           <CollapsibleItem header='Symptomatic Experience' icon='brightness_2'>
             {this.props.phase.symptomatic_experience}
           </CollapsibleItem>
-
-          <CollapsibleItem header='Mindful Ways' icon='brightness_3'>
-
-            {this.props.phase.mindful_ways}
+          <CollapsibleItem header='Mindful Ways' icon='brightness_3'>{this.props.phase.mindful_ways}
           </CollapsibleItem>
-
         </Collapsible>
-
       </Modal>
      )
    }
